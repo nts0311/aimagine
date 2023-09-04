328006ff962c3bfeb1a531ca4fc2bf00e3807ac4
sonnt <sonnt@biplus.com.vn>
2023-09-04 20:54:26 +0700

    Initial Commit

---
+++ aimagine/aimagineApp.swift
@@ -0,0 +1,17 @@
+//
+//  aimagineApp.swift
+//  aimagine
+//
+//  Created by son on 04/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct aimagineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
